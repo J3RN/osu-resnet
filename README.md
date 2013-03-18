4835891322262168aafaa5574783baeaf09f8717
Jon Arnett <jonarnett90@gmail.com>
2013-03-18 19:04:30 -0400

    Semi-working code

---
+++ README.md
@@ -0,0 +1,4 @@
+OSUResnetContinuousConnection
+=============================
+
+A simple script to allow computers connected to Resnet to have a connection without being disconnected every 24 hours.
---
+++ connect.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import httplib, urllib
+import socket
+from time import sleep
+
+
+def connect():    
+    headers = {"Content-type":"application/x-www-form-urlencoded"}
+    
+    ip = get_local_ip()    
+    
+    params = urllib.urlencode({
+        'reqFrom':'perfigo_login.jsp',
+        'uri':'',
+        'cm':'',
+        'userip':'\'' + ip + '\'',
+        'session':'',
+        'pm':'',
+        'index':'1',
+        'pageid':'-1',
+        'compact':'false',
+        
+        'registerGuest':'NO',
+        'userNameLabel':'OSU name.# (lowercase)',
+        'passwordLabel':'Password',
+        'guestUserNameLabel':'Guest ID',
+        'guestPasswordLabel':'Password',
+        
+        'username':'\'' + username + '\'',
+        'password':'\'' + password + '\'',
+        
+        'provider':'OSU username'
+    })
+    
+    print "IP is: " + ip
+
+    # Connection request
+    h = httplib.HTTPSConnection("be4cas03.resnet.ohio-state.edu")   
+    
+    h.request("GET", "/auth/perfigo_weblogin.jsp")    
+    
+    print h.getresponse().read()    
+    
+    h.request("POST", "/auth/perfigo_cm_validate.jsp", params, headers)
+
+    print h.getresponse().read()
+    
+    # Google test
+    h2 = httplib.HTTPConnection("www.google.com")
+
+    h2.request("GET", "/")
+
+    data = h2.getresponse().read()
+    
+    # Test to see if a real reply from Google was recieved
+    if not "ohio-state" in data:
+        print "Successfully Connected!"
+    else:
+        print "Connection failed"
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    host = "www.google.com"
+    
+    try:
+        s.connect((host, 9))
+        client = s.getsockname()[0]
+    except socket.error:
+        client = "Unknown IP"
+    finally:
+        del s
+        
+    return client
+
+# Start of program
+
+username = raw_input("What is your OSU Name.#? ")
+password = raw_input("What is your OSU password? (Beware of those standing behind you) ")
+print ""
+cont = raw_input("Run continuously? (Y/n) ")
+print ""
+
+if cont.capitalize() == "Y":
+    while True:
+        connect()
+        sleep(23 * 60 * 60) # 23 hours in seconds
+else:
+    connect()
